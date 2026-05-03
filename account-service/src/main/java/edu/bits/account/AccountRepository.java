7cf7c1f0e9b8a1b928bf213dbea851decb86c97d
KollaNagaManasa <2024tm93519@wilp.bits-pilani.ac.in>
2026-05-03 12:03:40 +0530

    Delete account-service directory

--- account-service/src/main/java/edu/bits/account/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.bits.account;
-
-import java.util.Optional;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-    Optional<Account> findByAccountNumber(String accountNumber);
-}
